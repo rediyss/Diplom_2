c33710f07bc4bd7732e8e5da83af5a13d81a56fd
rediyss <reddreynoklik@yandex.ru>
2025-08-06 13:07:55 +1100

    Вынесены в отдельный класс шаги для использования в разных тестах

---
+++ src/test/java/steps/UserSteps.java
@@ -0,0 +1,46 @@
+package steps;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class UserSteps {
+
+    @Step("Генерация уникального email")
+    public String generateUniqueEmail() {
+        return "user" + System.currentTimeMillis() + "@test.com";
+    }
+
+    @Step("Регистрация пользователя: {payload}")
+    public Response registerUser(String payload) {
+        return given()
+                .header("Content-type", "application/json")
+                .body(payload)
+                .when()
+                .post("/api/auth/register");
+    }
+
+    @Step("Удаление пользователя по accessToken")
+    public void deleteUser(String accessToken) {
+        given()
+                .header("Authorization", "Bearer " + accessToken)
+                .when()
+                .delete("/api/auth/user")
+                .then()
+                .statusCode(202); // см. документацию — может быть другой код
+    }
+
+    @Step("Извлечение accessToken из ответа")
+    public String extractAccessToken(Response response) {
+        return response.jsonPath().getString("accessToken").replace("Bearer ", "");
+    }
+    @Step("Логин пользователя: {payload}")
+    public Response loginUser(String payload) {
+        return given()
+                .header("Content-type", "application/json")
+                .body(payload)
+                .when()
+                .post("/api/auth/login");
+    }
+}
